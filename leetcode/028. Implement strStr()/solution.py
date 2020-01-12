57507b357f7a22ddfd6ecb6f1ebad7897a30d64f
FranckNdame <franck.mpouli@yahoo.com>
2020-01-12 01:03:23 +0000

    leetcode - 028

---
+++ leetcode/028. Implement strStr()/solution.py
@@ -0,0 +1,16 @@
+def strStr(self, haystack: str, needle: str) -> int:
+    if not needle:
+        return 0
+    if haystack == needle:
+        return 0
+
+    lenH = len(haystack)
+    lenN = len(needle)
+    if lenN >= lenH:
+        return -1
+
+    for i in range(lenH - lenN + 1):
+        if haystack[i:i+lenN] == needle:
+            return i
+
+    return -1
