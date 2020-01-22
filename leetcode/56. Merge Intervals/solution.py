2830336fd3e9725a95d6ecae46e3a7671e50cd60
FranckNdame <franck.mpouli@yahoo.com>
2020-01-22 13:58:52 +0000

    56 - leetcode

---
+++ leetcode/56. Merge Intervals/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    # Time Complexity: O(nlogn) || Space Complexity: O(n)
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals: return None
+        intervals.sort(key=lambda x: x[0])
+        result = []
+        
+        for interval in intervals:
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1], interval[1])
+                
+        return result
+    
+
+                    
+        
