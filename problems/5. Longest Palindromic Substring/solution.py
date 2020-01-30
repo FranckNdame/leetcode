5eaed310d3864bfa4503d245da1da24ba9b16c23
FranckNdame <franck.mpouli@yahoo.com>
2020-01-30 22:47:03 +0000

    5 - leetcode

--- problems/5. Longest Palindromic Substring/solution.py
+++ problems/5. Longest Palindromic Substring/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1: return s
-        longest = ""
-        for i in range(1,len(s)):
+        if len(s) == 1:
+            return s
+        longest = [0, 0]
+        for i in range(1, len(s)):
             odd = self.expandString(i-1, i+1, s)
             even = self.expandString(i-1, i, s)
-            localLongest = max(odd, even, key=lambda x:len(x))
-            longest = max(longest, localLongest, key=lambda x:len(x))
-        return longest
-            
-            
+            localLongest = max(odd, even, key=lambda x: x[1]-x[0])
+            longest = max(longest, localLongest, key=lambda x: x[1]-x[0])
+        return s[longest[0]:longest[1]]
+
     def expandString(self, i, j, s):
         while i >= 0 and j < len(s):
-            if s[i] != s[j]: break
+            if s[i] != s[j]:
+                break
             i -= 1
             j += 1
-        return  s[i+1:j]
-        
+        return [i+1, j]
