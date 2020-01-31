a47df239e3906763e6ede8a755e2d06ecf8f7d6e
FranckNdame <franck.mpouli@yahoo.com>
2020-01-31 11:45:03 +0000

    268 - leetcode

--- problems/268. Missing Number/solution.py
+++ problems/268. Missing Number/solution.py
@@ -1,10 +1,6 @@
 class Solution:
 
     def missingNumber(self, nums: List[int]) -> int:
-        x, y = 0, 0
-        
-        for i in range(len(nums)+1): x ^= i
-        for num in nums: y ^= num
-            
-        return x^y
-        
+        n = len(nums)
+        for i in range(n): n ^= i ^ nums[i]
+        return n        
