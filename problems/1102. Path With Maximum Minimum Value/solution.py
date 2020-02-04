d0766f7fb047e93f8b2b825f2e774a244625d4df
FranckNdame <franck.mpouli@yahoo.com>
2020-02-04 16:06:24 +0000

    1102 - leetcode

--- problems/1102. Path With Maximum Minimum Value/solution.py
+++ problems/1102. Path With Maximum Minimum Value/solution.py
@@ -1,39 +1,16 @@
 class Solution:
-    def maximumMinimumPath(self, A: [[int]]) -> int:
-        visited = {}
-        self.dfs((0, 0), A, {}, float('inf'), [])
+    def maximumMinimumPath(self, matrix: List[List[int]]) -> int:
+        target = (len(matrix)-1, len(matrix[0])-1)
+        heap = [(-matrix[0][0], (0, 0))]
+        visited = {(0, 0): True}
 
-    def dfs(self, point, grid, visited, minVal, path):
-        row, col = point
-        currVal = grid[row][col]
-        # print(point, currVal)
-        minVal = min(minVal, currVal)
-        visited[point] = True
-        path.append(currVal)
-
-        # print(f"Value: {currVal} || Point: {point} || V: {visited} || m: {minVal}")
-        if point == (len(grid)-1, len(grid[0])-1):
-            print(f"Goal reached ==> minVal: {minVal} \npath: {path} \n\n")
-        n = []
-        for nr, nc in [(row+1, col), (row-1, col), (row, col+1), (row, col-1)]:
-            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr, nc) not in visited:
-                n.append((nr, nc))
-
-        for nr, nc in list(reversed(sorted(n, key=lambda x: grid[x[0]][x[1]]))):
-            visited[(nr, nc)] = True
-            self.dfs((nr, nc), grid, visited.copy(), minVal, path[:])
-            # n.append((nr, nc))
-
-
-grid = [
-    [5, 4, 5],
-    [1, 2, 6],
-    [7, 4, 6]
-]
-solution = Solution()
-solution.maximumMinimumPath(
-    [[2, 0, 5, 2, 0],
-     [2, 4, 4, 4, 3],
-     [1, 5, 0, 0, 0],
-     [5, 4, 4, 3, 1],
-     [1, 3, 1, 5, 3]])
+        while heap:
+            node, coord = heapq.heappop(heap)
+            if coord == target:
+                return abs(node)
+            r, c = coord
+            for nr, nc in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
+                if 0 <= nr < len(matrix) and 0 <= nc < len(matrix[0]) and (nr, nc) not in visited:
+                    visited[(nr, nc)] = True
+                    nextNode = -matrix[nr][nc]
+                    heapq.heappush(heap, (max(node, nextNode), (nr, nc)))
