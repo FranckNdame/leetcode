306849c6eb91f776e65359b9de9046f574b08ad2
FranckNdame <franck.mpouli@yahoo.com>
2020-02-08 14:13:01 +0000

    22 - leetcode

--- problems/22. Generate Parentheses/solution.py
+++ problems/22. Generate Parentheses/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        self.helper(["(", ")"], n, result)
-        return list(set(result))
+        self.backtrack(n, result)
+        return result
 
-    def helper(self, brackets, n, result):
-        if len(brackets) == n*2:
-            result.append("".join(brackets))
-            return
-
-        for i in range(len(brackets)):
-            self.helper(brackets[:i] + ["("] + [")"] + brackets[i:], n, result)
+    def backtrack(self, n, result, string="", left=0, right=0):
+        if len(string) == n*2:
+            result.append(string)
+        if left < n:
+            self.backtrack(n, result, string + "(", left+1, right)
+        if right < left:
+            self.backtrack(n, result, string + ")", left, right+1)
--- problems/playground.py
+++ problems/playground.py
@@ -1,14 +1,1 @@
-def isSimilar(wordOne, wordTwo):
-    wordOne, wordTwo = list(wordOne), list(wordTwo)
-    for i in range(len(wordTwo)):
-        for j in range(i, len(wordTwo)):
-            wordTwo[i], wordTwo[j] = wordTwo[j], wordTwo[i]
-            if wordOne == wordTwo:
-                return True
-            wordTwo[i], wordTwo[j] = wordTwo[j], wordTwo[i]
-    return False
-
-
-# print(isSimilar("arts", "tars"))
-
-print(553 % 500)
+print("NELSON")
