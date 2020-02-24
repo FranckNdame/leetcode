0e10e842f5d76a1e20b100e9a37baa3f7ce0e107
FranckNdame <franck.mpouli@yahoo.com>
2020-02-24 12:12:37 +0000

    leetcode - 78 - apprch 2

--- problems/78. Subsets/solution.py
+++ problems/78. Subsets/solution.py
@@ -15,3 +15,15 @@ class Solution:
         for i in range(len(nums)):
             newNums = nums[:i] + nums[i+1:]
             self.getSubsets(newNums, lookup, result)
+
+# Approach 2 (best)
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            count = len(result)
+            for i in range(count):
+                result.append(result[i] + [num])
+        return result
