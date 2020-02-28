61e2d60856ca938748221735acbc453d4f0d342f
FranckNdame <franck.mpouli@yahoo.com>
2020-02-28 08:51:41 +0000

    leetcode - 678 - fix

--- problems/678. Valid Parenthesis String/solution.py
+++ problems/678. Valid Parenthesis String/solution.py
@@ -3,8 +3,8 @@ class Solution:
         low = hi = 0
         for c in s:
             low += 1 if c == "(" else -1
-            hi += 1 if c == ")" else -1
+            hi += 1 if c != ")" else -1
             if hi < 0:
                 break
-            low = max(low, hi)
+            low = max(low, 0)
         return low == 0
