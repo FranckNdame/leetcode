1ebec14ecae4e6de3cad3848fe29ef054d4a2494
FranckNdame <franck.mpouli@yahoo.com>
2020-03-03 13:55:22 +0000

    leetcode - 28 - kmp algorithm

--- problems/028. Implement strStr()/solution.py
+++ problems/028. Implement strStr()/solution.py
@@ -1,14 +1,31 @@
-def strStr(self, haystack: str, needle: str) -> int:
-    if not needle:return 0
-    if haystack == needle: return 0
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if haystack == needle:
+            return 0
+        if len(needle) >= len(haystack):
+            return -1
 
-    lenH = len(haystack)
-    lenN = len(needle)
-    if lenN >= lenH:
+        prefix_table = self.constructTable(needle)
+        j = 0
+        for i in range(len(haystack)):
+            while j > 0 and haystack[i] != needle[j]:
+                j = prefix_table[j-1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i - j + 1
+
         return -1
 
-    for i in range(lenH - lenN + 1):
-        if haystack[i:i+lenN] == needle:
-            return i
-
-    return -1
+    def constructTable(self, pattern: str) -> [int]:
+        table = [0] * len(pattern)
+        j = 0
+        for i in range(1, len(pattern)):
+            while j > 0 and pattern[i] != pattern[j]:
+                j = table[j-1]
+            if pattern[i] == pattern[j]:
+                j += 1
+            table[i] = j
+        return table
