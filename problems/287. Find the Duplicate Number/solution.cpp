a100a9bb970f35c38b25453ae0cf3b9faafb30d9
FranckNdame <franck.mpouli@yahoo.com>
2020-03-05 23:19:49 +0000

    leetcode - 287

---
+++ problems/287. Find the Duplicate Number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+                return nums[i];
+        }
+
+        return -1;
+    }
+};
